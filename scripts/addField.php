995c038b5983958de599f6a40dc43feddfd24c05
1918.NC.GEARS.webDev <codesquirrel75@gmail.com>
2018-02-14 18:42:14 -0500

    add field

--- scripts/addField.php
+++ scripts/addField.php
@@ -6,8 +6,13 @@ include 'functions.php';
 
 $type = $_POST['field'];
 $fieldNumber = fieldNumber();
-$properties = array('fieldName'=>'<input type="text" placeholder="Field ' . $fieldNumber . '" size="15" name="fieldName"> ', 'fieldType'=>$type);
+$properties = array('fieldName'=>'Field Name', 'fieldType'=>$type, 'fieldKey'=>'FieldKey', 'hideFieldLabel'=>False, 'hidden'=>False);
 
+if($type == "listselector")
+{
+	$var = array('hintText'=>'Hint Text', 'allowMultiSelection'=>True, 'fieldRules'=>'Field Rules', 'required'=>False, 'captureTimeStamp'=>False, 'values'=>'Values');
+	$properties = array_merge($properties, $var);
+}
 if($type == "text")
 {
 	$var = array('blubergub'=>'Flubers', 'visible'=>'True');
@@ -16,23 +21,28 @@ if($type == "text")
 
 if($type == "statictext")
 {
-
+	$var = array('blubergub'=>'Flubers', 'visible'=>'True');
+	$properties = array_merge($properties, $var);
 }
 if($type == "numeric")
 {
-
+	$var = array('blubergub'=>'Flubers', 'visible'=>'True');
+	$properties = array_merge($properties, $var);
 }
 if($type == "photo")
 {
-
+	$var = array('blubergub'=>'Flubers', 'visible'=>'True');
+	$properties = array_merge($properties, $var);
 }
 if($type == "optionlist")
 {
-
+	$var = array('blubergub'=>'Flubers', 'visible'=>'True');
+	$properties = array_merge($properties, $var);
 }
 if($type == "yesnona")
 {
-
+	$var = array('blubergub'=>'Flubers', 'visible'=>'True');
+	$properties = array_merge($properties, $var);
 }
 
 $_SESSION['form']['pages'][$_SESSION['selectedPage']]['sections'][$_SESSION['selectedSection']]['fields']['field' . $fieldNumber] = $properties;
